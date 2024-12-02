257740c088a9522031bd6791082e5394c10e712c
Chris Panas <chris@DESKTOP-RK93M8P>
2024-12-02 17:31:36 -0500

    Updated for graphing

--- network.py
+++ network.py
@@ -4,7 +4,10 @@ import util
 import main
 import matplotlib.pyplot as plt # type: ignore
 
-def gradient_descent(X, Y, layers, iterations, alpha):
+def gradient_descent(X, Y, layers, iterations, alpha, capture_rate):
+
+    captures = []
+
     for i in range(iterations):
         Z, A = forward(layers, X)
         dW, db = backprop(layers, Z, A, X, Y)
@@ -13,11 +16,15 @@ def gradient_descent(X, Y, layers, iterations, alpha):
         if main.progress_bar is True:
             util.progress_bar(i, iterations)
 
+        #capture the final A values
+        if capture_rate > 0 and i % capture_rate == 0:
+            captures.append(util.get_accuracy(util.get_predictions(A[len(A) - 1]), Y))
+
     if main.progress_bar is True:
         util.progress_bar(iterations, iterations)
 
     print("Trained Accuracy: ", util.get_accuracy(util.get_predictions(A[len(A) - 1]), Y))
-    return layers
+    return layers, captures
 
 #Used to get accuracy on the test set
 def test_set(X, Y, layers):
@@ -36,6 +43,20 @@ def visual_test_set(X, Y, layers, iterations, time):
         print("Predicted: {} Actual: {}".format(labels[util.get_predictions(A[len(A) - 1])[i]], labels[Y[i]]))
         util.rebuild_image(X.T[i], time)
     plt.close()
+
+#Used to plot accuracy over time
+def plot_test_set(X, Y, layers, captures, capture_rate):
+    capture_accuracy = captures
+    capture_iteration = []
+
+    for i in range(0, len(captures)):
+        capture_iteration.append(i * capture_rate)
+
+    plt.plot(capture_iteration, capture_accuracy)
+    plt.xlabel("Iterations")
+    plt.ylabel("Accuracy")
+    plt.title("Accuracy over Iterations")
+    plt.show()
 
 def forward(layers, X):
     A = [] #activation step
@@ -79,18 +100,17 @@ def backprop(layers, Z, A, X, Y):
     db.append(1 / m * np.sum(dZ[0]))
 
     #additonal layers
-    #TODO get this to work
     for i in range(1, top):
         dZ.append(layers[top - i + 1].weight.T.dot(dZ[i - 1]) * act.get_d_activation(layers[top - i].activation, Z[top - i]))
         dW.append(1 / m * dZ[i].dot(Z[top - i - 1].T))
         db.append(1 / m * np.sum(dZ[i]))
 
-    #output
+    #output layers
     dZ.append(layers[1].weight.T.dot(dZ[top - 1]) * act.get_d_activation(layers[0].activation, Z[0]))
     dW.append(1 / m * dZ[top].dot(X.T))
     db.append(1 / m * np.sum(dZ[top]))
 
-    #backprop is done backwards - reverse for relation
+    #backprop is done backwards - reverse for relation (in update params)
     dW.reverse()
     db.reverse()
     return dW, db
